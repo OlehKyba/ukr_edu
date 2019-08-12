4afdf440e5560717e9363fd14d558dbd26b4561b
Oleh Kyba <vestigor0@gmail.com>
2019-08-12 16:37:56 +0300

    Added tag model and relationship many-to-many between tag and post.

--- app/models.py
+++ app/models.py
@@ -1,6 +1,14 @@
 from .extentions import db
 
 
+tags = db.Table('tags',
+                db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'),
+                          primary_key=True),
+                db.Column('post_id', db.Integer, db.ForeignKey('post.id'),
+                          primary_key=True)
+                )
+
+
 class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
@@ -20,3 +28,12 @@ class Post(db.Model):
     subtitle = db.Column(db.String(240), nullable=False)
     text = db.Column(db.Text, nullable=False)
     author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    tags = db.relationship('Tag', secondary=tags, lazy='subquery',
+                           backref=db.backref('posts', lazy=True))
+
+
+class Tag(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    value = db.Column(db.String(100), nullable=False, unique=True)
