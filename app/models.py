c555d78068dcd238cebefc21280e5841e400605e
Oleh Kyba <vestigor0@gmail.com>
2019-08-12 15:43:42 +0300

    Add post model.

--- app/models.py
+++ app/models.py
@@ -7,3 +7,11 @@ class User(db.Model):
     name = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), nullable=True)
+
+
+class Post(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), unique=True, nullable=False)
+    subtitle = db.Column(db.String(240), nullable=False)
+    text = db.Column(db.Text, nullable=False)
