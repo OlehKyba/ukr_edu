1e76114a843c3d65353b909e679b167267ffb220
Oleh Kyba <vestigor0@gmail.com>
2019-08-16 15:09:47 +0300

    Modified the carousel display on the index page.

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,10 @@
 from flask import current_app, render_template
+from .models import Post
 
 
 @current_app.route("/")
 def index():
-    return render_template('index.html', current_user=False)
+    posts = Post.query.order_by(Post.date.desc()).limit(3)
+    for i in posts:
+        print(i.title, i.date)
+    return render_template('index.html', current_user=False, posts=posts)
