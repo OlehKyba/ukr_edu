beb3bb96bb1aaf979c203b4db2183d4983904a6f
Oleh Kyba <vestigor0@gmail.com>
2019-08-16 15:37:27 +0300

    Added title.

--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,4 @@ from .models import Post
 @current_app.route("/")
 def index():
     posts = Post.query.order_by(Post.date.desc()).limit(3)
-    for i in posts:
-        print(i.title, i.date)
     return render_template('index.html', current_user=False, posts=posts)
