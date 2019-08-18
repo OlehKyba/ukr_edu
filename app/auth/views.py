2c46bbe03ede167f71b87cb3df81b6dd23e8ce64
Oleh Kyba <vestigor0@gmail.com>
2019-08-18 18:06:44 +0300

    Modified the display of the login page and added the transition to the next page.

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,4 +1,5 @@
-from flask import render_template, redirect, flash, url_for, Markup
+from werkzeug.urls import url_parse
+from flask import render_template, redirect, flash, url_for, request
 from flask.views import MethodView
 from flask_login import current_user, login_user
 
@@ -18,8 +19,11 @@ class LoginView(MethodView):
 
     def post(self):
         form = LoginForm()
+        next_page = url_for('index')
+
         if current_user.is_authenticated:
             return redirect(url_for('index'))
+
         if form.validate_on_submit():
             user = User.query.filter_by(name=form.name.data).first()
 
@@ -30,12 +34,17 @@ class LoginView(MethodView):
 
                 flash(invalid_message, category='danger')
 
-                return redirect(url_for('index'))
+                return redirect(url_for('auth.login'))
+
             login_user(user, remember=form.is_remember.data)
+
+            possibly_next = request.args.get('next')
+            if possibly_next and url_parse(possibly_next).netloc == '':
+                next_page = possibly_next
 
             success_message = Message('Ви зайшли на акаунт ').strong(
                 current_user.name).text('!').result()
 
             flash(success_message, 'success')
 
-        return redirect(url_for('index'))
+        return redirect(next_page)
