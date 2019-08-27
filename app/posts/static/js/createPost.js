578973b885236d193dcae1db7abc593ef651c9fd
Oleh Kyba <vestigor0@gmail.com>
2019-08-27 19:20:20 +0300

    Added html create/update post representation.

---
+++ app/posts/static/js/createPost.js
@@ -0,0 +1,20 @@
+
+
+const spanFileName = document.getElementById("fileName");
+
+const inputImageFile = document.getElementById("inputImageFile");
+
+const inputDate = document.getElementById("inputDate");
+
+
+const getToday = () => {
+    const today = new Date();
+    return today.toISOString().substr(0, 10);
+};
+
+
+inputImageFile.addEventListener('input', () => (spanFileName.innerHTML = inputImageFile.value, console.log(inputImageFile.value)));
+inputDate.value = getToday();
+
+
+
