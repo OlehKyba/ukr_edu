041c56dc8ec876414feb923b9b12dccca98e4446
Oleh Kyba <vestigor0@gmail.com>
2019-08-27 19:04:42 +0300

    Added SlugMixin for Post and Tag models.

--- app/models.py
+++ app/models.py
@@ -3,6 +3,8 @@ from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
+from app.sa_slug import SlugMixin
+
 
 roles = db.Table('roles',
                  db.Column('user_id', db.Integer, db.ForeignKey('user.id'),
@@ -46,7 +48,7 @@ class Role(db.Model):
     name = db.Column(db.String(50), unique=True, nullable=False)
 
 
-class Post(db.Model):
+class Post(db.Model, SlugMixin('title', 120)):
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120), unique=True, nullable=False)
@@ -54,13 +56,13 @@ class Post(db.Model):
     text = db.Column(db.Text, nullable=False)
     image = db.Column(db.String(100), nullable=True)
     date = db.Column(db.DateTime, default=datetime.now())
-    author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     tags = db.relationship('Tag', secondary=tags, lazy='subquery',
                            backref=db.backref('posts', lazy=True))
 
 
-class Tag(db.Model):
+class Tag(db.Model, SlugMixin('value', 100)):
 
     id = db.Column(db.Integer, primary_key=True)
     value = db.Column(db.String(100), nullable=False, unique=True)
