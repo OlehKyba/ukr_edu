6de87a347f96d7f0f43012df6839fc2e57198d79
Oleh Kyba <vestigor0@gmail.com>
2019-08-30 15:09:01 +0300

    The logics of adding tags and changing model fields depending on the form are separated.

--- app/posts/views.py
+++ app/posts/views.py
@@ -2,7 +2,7 @@ from flask import render_template, flash, redirect, url_for, request
 
 from .forms import PostForm
 
-from app.models import Post, Tag
+from app.models import Post, Tag, get_or_create
 from app.extentions import db
 
 from sqlalchemy.orm import subqueryload
@@ -17,14 +17,17 @@ def post_action(strategy_factory):
         strategy = strategy_factory()
         post = strategy.post_factory(slug)
         post_form = PostForm(obj=post)
-        post_form.tags.query = Tag.query.options(
-            subqueryload(Tag.posts)).filter(Post.id == post.id)
 
-        if post_form.validate_on_submit()
-        and request.method == strategy.method:
+        if request.method == 'GET':
+            tags = [tag.value for tag in post.tags]
+            post_form.tags.data = ','.join(tags)
+
+        if post_form.validate_on_submit() and request.method == 'POST':
+            values = post_form.tags.data.split(',')
+            post_form.tags.data = [get_or_create(Tag, value=value)
+                                   for value in values]
 
             post_form.populate_obj(post)
-            # TODO: Normal tag saving.
             db.session.add(post)
             db.session.commit()
 
@@ -39,7 +42,6 @@ def post_action(strategy_factory):
 
 PostStrategy = namedtuple('PostStrategy',
                           ['post_factory',
-                           'method',
                            'next_page',
                            'message'])
 
@@ -51,7 +53,6 @@ def create_post():
         post_factory=lambda slug: Post(),
         next_page=lambda post: url_for('posts_bp.post', slug=post.slug),
         message=('Створенно новий пост!', 'success'),
-        method='POST',
     )
     return create_strategy
 
@@ -64,7 +65,6 @@ def update_post():
             slug=slug).first_or_404(),
         next_page=lambda post: url_for('posts_bp.post', slug=post.slug),
         message=('Змінено цей пост!', 'primary'),
-        method='POST',
     )
 
     return update_strategy
