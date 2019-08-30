2e46b480db5e5334f95221359b953453899891f3
Oleh Kyba <vestigor0@gmail.com>
2019-08-30 15:01:54 +0300

    Rewritten post creation and update form.

--- app/posts/forms.py
+++ app/posts/forms.py
@@ -1,23 +1,59 @@
 from flask_wtf import FlaskForm
 
-from wtforms import StringField, TextAreaField, BooleanField, FileField, SubmitField
+from wtforms import (StringField,
+                     TextAreaField,
+                     BooleanField,
+                     FileField,
+                     SelectMultipleField,
+                     SubmitField)
+
 from wtforms.fields.html5 import DateField
 from wtforms.validators import DataRequired, Length
-from wtforms.ext.sqlalchemy.fields import QuerySelectMultipleField
+from wtforms.ext.sqlalchemy.orm import model_form
+
+from datetime import datetime
 
 from app.markup_messages import Message
-from app.models import Tag
+from app.models import Post
+from app.extentions import db
 
 
-class PostForm(FlaskForm):
-
-    title = StringField('Заголовок', validators=[
-                        DataRequired(), Length(max=120)])
-    subtitle = StringField('Підзаголовок', validators=[
-                           DataRequired(), Length(max=240)])
-    text = TextAreaField('Оснонвний текст статті', validators=[DataRequired()])
-    tags = QuerySelectMultipleField(
-        'Введіть теги:', query_factory=lambda: Tag.query, get_pk=lambda tag: tag.value, allow_blank=True,)
-    image = FileField(Message().span('Виберіть файл', id='fileName').result())
-    date = DateField('Дата публікації:')
+class _SubmitTagDateMixin(FlaskForm):
     submit = SubmitField('OK')
+    tags = StringField('Введіть теги:', default='')
+    date = DateField('Дата публікації:', default=datetime.now())
+
+
+settings = {
+    'title': {
+        'label': 'Заголовок',
+        'validators': [
+            DataRequired(),
+            Length(max=120),
+        ],
+    },
+
+    'subtitle': {
+        'label': 'Підзаголовок',
+        'validators': [
+            DataRequired(),
+            Length(max=240),
+        ],
+    },
+
+    'text': {
+        'label': 'Оснонвний текст статті',
+        'validators': [
+            DataRequired(),
+        ],
+    },
+
+    'image': {
+        'label': Message().span('Виберіть файл', id='fileName').result(),
+    },
+}
+
+PostForm = model_form(Post,
+                      db_session=db.session,
+                      base_class=_SubmitTagDateMixin,
+                      field_args=settings, exclude=['tags', 'date'])
