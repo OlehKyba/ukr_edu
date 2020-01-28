ca904757501a64d0be87546f161de6c9374bdc87
Oleh Kyba <vestigor0@gmail.com>
2020-01-28 19:32:24 +0200

    The post form was separated from the postal model.

--- app/posts/forms.py
+++ app/posts/forms.py
@@ -1,25 +1,17 @@
 from flask_wtf import FlaskForm
 
-from wtforms import (StringField,
-                     TextAreaField,
-                     BooleanField,
-                     FileField,
-                     SelectMultipleField,
-                     SubmitField)
+from wtforms import StringField, TextAreaField, FileField, SubmitField
 from flask_wtf.file import FileAllowed
 
 from wtforms.fields.html5 import DateField
 from wtforms.validators import DataRequired, Length
-from wtforms.ext.sqlalchemy.orm import model_form
 
 from datetime import datetime
 
 from app.utils.markup_messages import Message
-from app.models import Post
-from app.extentions import db
 
 
-class _SubmitTagDateMixin(FlaskForm):
+class PostForm(FlaskForm):
     submit = SubmitField('OK')
     tags = StringField('Введіть теги:', default='')
     date = DateField('Дата публікації:', default=datetime.now())
@@ -32,39 +24,16 @@ class _SubmitTagDateMixin(FlaskForm):
         'До статті можна прикріпити лише зображення!',
     )])
 
+    title = StringField('Заголовок', validators=[
+        DataRequired(),
+        Length(max=120),
+    ])
 
-settings = {
-    'title': {
-        'label': 'Заголовок',
-        'validators': [
+    subtitle = StringField('Підзаголовок', validators=[
+        DataRequired(),
+        Length(max=120),
+    ])
+
+    text = TextAreaField('Оснонвний текст статті', validators=[
             DataRequired(),
-            Length(max=120),
-        ],
-    },
-
-    'subtitle': {
-        'label': 'Підзаголовок',
-        'validators': [
-            DataRequired(),
-            Length(max=240),
-        ],
-    },
-
-    'text': {
-        'label': 'Оснонвний текст статті',
-        'validators': [
-            DataRequired(),
-        ],
-    },
-}
-
-PostForm = model_form(Post,
-                      db_session=db.session,
-                      base_class=_SubmitTagDateMixin,
-                      field_args=settings,
-                      exclude=[
-                          'tags',
-                          'date',
-                          '_image',
-                      ],
-                      )
+    ])
