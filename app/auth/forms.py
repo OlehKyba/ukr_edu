22f83bb04ae79c356cfc125530de2d78f68f6926
Oleh Kyba <vestigor0@gmail.com>
2020-02-13 20:38:42 +0200

    Redesigning the login form and adding support for recaptcha.

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,11 +1,17 @@
-from flask_wtf import FlaskForm
+from flask_wtf import FlaskForm, RecaptchaField
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, Length
 
 
 class LoginForm(FlaskForm):
 
-    name = StringField('Логін', validators=[DataRequired(), Length(max=50)])
-    password = PasswordField('Пароль', validators=[DataRequired()])
+    name = StringField('Логін', validators=[
+        DataRequired(),
+        Length(max=50, message="Ім'я користувача має бути не більше 50 символів!"),
+    ])
+    password = PasswordField('Пароль', validators=[
+        DataRequired(),
+    ])
     is_remember = BooleanField("Запам'ятайте мене")
+    recaptcha = RecaptchaField()
     submit = SubmitField('Вхід')
